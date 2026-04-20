6c66948a241108984a9a59e2707a430125717f40
Gazi Ul Islam Allie <ibnibilal114@gmail.com>
2026-04-20 17:56:01 +0530

    done

--- AsyncWait.cs
+++ AsyncWait.cs
@@ -1,159 +1,292 @@
+// // // // // using System;
+// // // // // using System.Threading.Tasks;
+// // // // // public class PlyerPf
+// // // // // {
+// // // // //     public string Username { get; set; }
+// // // // //     public int Level { get; set; }
+
+// // // // // }
+
+
+// // // // // class Program
+// // // // // {
+// // // // //     static async Task Main()
+// // // // //     {
+// // // // //         Console.WriteLine("----------Game menue___");
+// // // // // Console.WriteLine("Fetching player profile...\n");
+// // // // // Task<PlyerPf> playerProfileTask = FetchPlayerProfileAsync("Gamer123");
+// // // // //         while (playerProfileTask.IsCompleted == false)
+// // // // //         {
+// // // // //             Console.WriteLine("Loading...");
+// // // // //             await Task.Delay(500);
+// // // // //         }
+
+// // // // //         PlyerPf profile = await playerProfileTask;
+// // // // //         Console.WriteLine($"Welcome back, {profile.Username}! You are currently at level {profile.Level}.");
+// // // // //     }
+
+// // // // //     static async Task<PlyerPf> FetchPlayerProfileAsync(string username)
+// // // // //     {
+// // // // //         await Task.Delay(1000);
+// // // // //         return new PlyerPf { Username = username, Level = 5 };
+// // // // //     }
+// // // // // }
+
+// // // // //  Generics book<T>  T == type 
+// // // // // / this is the type where we did not know what type of data is in the class or method it will only know when it runs the code 
+// // // // using System;
+// // // // using System.Security.Cryptography.X509Certificates;
+// // // // using System.Threading.Tasks;
+
+
+// // // // public class LootBox<T>
+// // // // {
+// // // //     private T _hiddenLoot;
+// // // //     public bool IsOpened { get; private set; }=false;
+// // // //     public LootBox(T loot)
+// // // //     {
+// // // //         _hiddenLoot = loot;
+// // // //     }
+// // // //     public async Task<T> Open()
+// // // //     {
+// // // //         if (IsOpened)
+// // // //         {
+// // // //             throw new InvalidOperationException("Loot box is already opened!");
+// // // //         }
+// // // //         Console.WriteLine("Opening loot box...");
+// // // //         await Task.Delay(3000);
+
+// // // //         IsOpened = true;    
+// // // //         Console.WriteLine("Loot box opened! Revealing loot...");
+// // // //         return _hiddenLoot;
+// // // //     }
+// // // // }
+// // // // class Program
+// // // // {
+
+
+// // // //     static void PrintAnything<T>(T item)
+// // // //     {
+// // // //         Console.WriteLine($" you gave me a {item.GetType().Name} with value of : {item}");
+// // // //     }
+
+
+// // // // static void Main()
+// // // // {
+// // // //     PrintAnything(42);
+// // // //     PrintAnything("Hello, World!");
+// // // //     PrintAnything(3.14);
+// // // //     PrintAnything(23.3434334);
+
+// // // //     LootBox<string> weaponBox = new LootBox<string>("Excalibur Sword");
+// // // //     LootBox<int> goldBox = new LootBox<int>(1000);
+// // // //     int gold = goldBox.Open().Result;
+// // // //     Console.WriteLine($"You received {gold} gold!");
+// // // //      string weapon = weaponBox.Open().Result;
+// // // //     Console.WriteLine($"You received the {weapon}!");
+
+// // // // }
+
+// // // // }
+
 // // // using System;
-// // // using System.Threading.Tasks;
-// // // public class PlyerPf
+
+// // // // RULE 1: Must be a static class!
+// // // // Standard naming convention is to add "Extensions" to the name.
+// // // public static class StringExtensions
 // // // {
-// // //     public string Username { get; set; }
-// // //     public int Level { get; set; }
+// // //     // RULE 2: Must be a static method!
+// // //     // RULE 3: The 'this string' tells C# to attach this to ALL strings everywhere!
+// // //     public static bool IsCapitalized(this string text)
+// // //     {
+// // //         if (string.IsNullOrEmpty(text)) return false;
 
+// // //         // Check if the first letter matches its uppercase version
+// // //         return text[0] == char.ToUpper(text[0]);
+// // //     }
+
+// // //     // Let's make another one that counts words!
+// // //     public static int WordCount(this string text)
+// // //     {
+// // //         // Split the string by spaces, and count how many pieces there are
+// // //         return text.Split(' ').Length;
+// // //     }
 // // // }
-
 
 // // // class Program
 // // // {
-// // //     static async Task Main()
+// // //     static void Main()
 // // //     {
-// // //         Console.WriteLine("----------Game menue___");
-// // // Console.WriteLine("Fetching player profile...\n");
-// // // Task<PlyerPf> playerProfileTask = FetchPlayerProfileAsync("Gamer123");
-// // //         while (playerProfileTask.IsCompleted == false)
-// // //         {
-// // //             Console.WriteLine("Loading...");
-// // //             await Task.Delay(500);
-// // //         }
+// // //         string myName = "Arthur";
+// // //         string sentence = "The quick brown fox jumps over the lazy dog";
 
-// // //         PlyerPf profile = await playerProfileTask;
-// // //         Console.WriteLine($"Welcome back, {profile.Username}! You are currently at level {profile.Level}.");
-// // //     }
+// // //         // LOOK AT THIS MAGIC! 
+// // //         // It looks like Microsoft wrote these methods!
+// // //         bool isCap = myName.IsCapitalized(); 
+// // //         int count = sentence.WordCount();
 
-// // //     static async Task<PlyerPf> FetchPlayerProfileAsync(string username)
-// // //     {
-// // //         await Task.Delay(1000);
-// // //         return new PlyerPf { Username = username, Level = 5 };
+// // //         Console.WriteLine($"Is '{myName}' capitalized? {isCap}");
+// // //         Console.WriteLine($"The sentence has {count} words.");
 // // //     }
 // // // }
 
-// // //  Generics book<T>  T == type 
-// // // / this is the type where we did not know what type of data is in the class or method it will only know when it runs the code 
+// // // // string builder 
 // // using System;
-// // using System.Security.Cryptography.X509Certificates;
-// // using System.Threading.Tasks;
+// // using System.Text; // REQUIRED!
 
-
-// // public class LootBox<T>
-// // {
-// //     private T _hiddenLoot;
-// //     public bool IsOpened { get; private set; }=false;
-// //     public LootBox(T loot)
-// //     {
-// //         _hiddenLoot = loot;
-// //     }
-// //     public async Task<T> Open()
-// //     {
-// //         if (IsOpened)
-// //         {
-// //             throw new InvalidOperationException("Loot box is already opened!");
-// //         }
-// //         Console.WriteLine("Opening loot box...");
-// //         await Task.Delay(3000);
-
-// //         IsOpened = true;    
-// //         Console.WriteLine("Loot box opened! Revealing loot...");
-// //         return _hiddenLoot;
-// //     }
-// // }
 // // class Program
 // // {
-    
-
-// //     static void PrintAnything<T>(T item)
+// //     static void Main()
 // //     {
-// //         Console.WriteLine($" you gave me a {item.GetType().Name} with value of : {item}");
+// //         Console.WriteLine("Building a massive string...");
+
+// //         // 1. Create the empty Whiteboard
+// //         StringBuilder myWhiteboard = new StringBuilder();
+
+// //         // 2. We can safely loop 10,000 times without crashing the memory!
+// //         for (int i = 0; i < 10000; i++)
+// //         {
+// //             myWhiteboard.Append("A"); // Instantly writes to the whiteboard
+// //         }
+
+// //         // 3. When we are 100% finished, we take a picture of the whiteboard 
+// //         // and convert it into a final, locked String (Stone Tablet).
+// //         string finalResult = myWhiteboard.ToString();
+
+// //         Console.WriteLine($"Finished! The string is {finalResult.Length} characters long.");
+// //         Console.WriteLine("the end  of");
 // //     }
-
-
-// // static void Main()
-// // {
-// //     PrintAnything(42);
-// //     PrintAnything("Hello, World!");
-// //     PrintAnything(3.14);
-// //     PrintAnything(23.3434334);
-
-// //     LootBox<string> weaponBox = new LootBox<string>("Excalibur Sword");
-// //     LootBox<int> goldBox = new LootBox<int>(1000);
-// //     int gold = goldBox.Open().Result;
-// //     Console.WriteLine($"You received {gold} gold!");
-// //      string weapon = weaponBox.Open().Result;
-// //     Console.WriteLine($"You received the {weapon}!");
-
 // // }
+// using System.ComponentModel.DataAnnotations;
+// using System.Data.Common;
+// using System.Dynamic;
 
-// // }
-
-// using System;
-
-// // RULE 1: Must be a static class!
-// // Standard naming convention is to add "Extensions" to the name.
-// public static class StringExtensions
+// public class Customer
 // {
-//     // RULE 2: Must be a static method!
-//     // RULE 3: The 'this string' tells C# to attach this to ALL strings everywhere!
-//     public static bool IsCapitalized(this string text)
-//     {
-//         if (string.IsNullOrEmpty(text)) return false;
+    
+//     private string _loyaltyId;
+//     public int Id  {get; set;}
+//     public string Name{get; set;}
+
+
+//     private string _email;
+//     public string Email
+//     { 
+//         get => _email;
+//         set
+//         {
+//             if(!value.Contains("@"))
+//             throw new ArgumentException("Invalid Email");
+//         }
         
-//         // Check if the first letter matches its uppercase version
-//         return text[0] == char.ToUpper(text[0]);
-//     }
 
-//     // Let's make another one that counts words!
-//     public static int WordCount(this string text)
-//     {
-//         // Split the string by spaces, and count how many pieces there are
-//         return text.Split(' ').Length;
 //     }
-// }
-
-// class Program
+//     public string DisplayName => $"{Id}:{Name}";
+//     }
+// public Customer()
 // {
-//     static void Main()
-//     {
-//         string myName = "Arthur";
-//         string sentence = "The quick brown fox jumps over the lazy dog";
-
-//         // LOOK AT THIS MAGIC! 
-//         // It looks like Microsoft wrote these methods!
-//         bool isCap = myName.IsCapitalized(); 
-//         int count = sentence.WordCount();
-
-//         Console.WriteLine($"Is '{myName}' capitalized? {isCap}");
-//         Console.WriteLine($"The sentence has {count} words.");
-//     }
+//     Id= GenerateId();
+// }
+// public Customer(string name, string email): this(){
+//    Name= name;
+//    Email =email;
 // }
 
-// // string builder 
-using System;
-using System.Text; // REQUIRED!
 
-class Program
+// Class definition with full features
+public class Customer
 {
-    static void Main()
-    {
-        Console.WriteLine("Building a massive string...");
-
-        // 1. Create the empty Whiteboard
-        StringBuilder myWhiteboard = new StringBuilder();
-
-        // 2. We can safely loop 10,000 times without crashing the memory!
-        for (int i = 0; i < 10000; i++)
+    // Fields (private by convention)
+    private string _loyaltyId;
+    
+    // Properties (auto-implemented)
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    // Property with validation
+    private string _email;
+    public string Email 
+    { 
+        get => _email;
+        set 
         {
-            myWhiteboard.Append("A"); // Instantly writes to the whiteboard
+            if (!value.Contains("@"))
+                throw new ArgumentException("Invalid email");
+            _email = value;
         }
-
-        // 3. When we are 100% finished, we take a picture of the whiteboard 
-        // and convert it into a final, locked String (Stone Tablet).
-        string finalResult = myWhiteboard.ToString();
-
-        Console.WriteLine($"Finished! The string is {finalResult.Length} characters long.");
-        Console.WriteLine("the end  of");
     }
+    
+    // Computed property
+    public string DisplayName => $"{Id}: {Name}";
+    
+    // Constructors
+    public Customer() // Parameterless
+    {
+        Id = GenerateId();
+    }
+    
+    public Customer(string name, string email) : this() // Constructor chaining
+    {
+        Name = name;
+        Email = email;
+    }
+    
+    // Static constructor (called once before first use)
+    static Customer()
+    {
+        Console.WriteLine("Customer class initialized");
+    }
+    
+    // Methods
+    public virtual void Save() // Virtual allows overriding
+    {
+        Console.WriteLine("Saving customer...");
+    }
+    
+    // Method overloading (compile-time polymorphism)
+    public void SendEmail(string subject) => SendEmail(subject, "Default body");
+    public void SendEmail(string subject, string body) 
+    {
+        // Implementation
+    }
+    
+    private int GenerateId() => new Random().Next(1000, 9999);
+}
+
+// Inheritance
+public class PremiumCustomer : Customer
+{
+    public decimal DiscountRate { get; set; }
+    
+    // Constructor calling base constructor
+    public PremiumCustomer(string name, string email, decimal discount) 
+        : base(name, email)
+    {
+        DiscountRate = discount;
+    }
+    
+    // Method overriding (runtime polymorphism)
+    public override void Save()
+    {
+        base.Save(); // Call base implementation
+        Console.WriteLine("Saving premium benefits...");
+    }
+}
+
+// Abstraction with abstract class
+public abstract class PaymentProcessor
+{
+    public abstract void ProcessPayment(decimal amount); // Must be implemented
+    
+    public void LogTransaction() // Concrete method
+    {
+        Console.WriteLine("Transaction logged");
+    }
+}
+
+// Interface (contract)
+public interface ILoggable
+{
+    string GetLogDetails();
+    DateTime LogDate { get; set; }
 }
--- obj/Debug/net10.0/c#.AssemblyInfo.cs
+++ obj/Debug/net10.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2a246c37463f185d32da64277c7a4b6b5c1be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4002cb7b4d5f581ca9163f223d86a5897dac1e12")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
